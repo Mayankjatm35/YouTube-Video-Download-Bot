018e03fbc4dfcce83d921ef4d6e71b936afe1d78
Mayankjatm35 <jatmayankjatm35@gmail.com>
2025-01-27 16:39:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/")
+def welcome():
+    return render_template("welcome.html")
 
 if __name__ == '__main__':
     # Run the Flask app on port 80
