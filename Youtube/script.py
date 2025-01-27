05cfb35c84f96d4c1ae28cd816c8d2c60c2d9c7e
Mayankjatm35 <jatmayankjatm35@gmail.com>
2025-01-27 16:34:01 +0530

    Add files via upload

---
+++ Youtube/script.py
@@ -0,0 +1,15 @@
+class Translation(object):
+
+    START_TEXT = '**Hello,** **{0}**  **{1}**\n\n**Send me the YouTube link of the video you want to upload**' 
+
+    ABOUT_TXT = """
+╭───────────⍟
+├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/https://t.me/YouTubeVideoDownloader0Bot)
+├📢 **Framework** : [PyroFork 2.3.50](https://docs.pyrogram.org/)
+├💮 **Language** : [Python 3.12.6](https://www.python.org)
+├👥 **Anime Channel** : [Warrior Units](https://t.me/Warrior_Units)
+├🥏 **Bot Update Channel** : [Warrior Units Bots CHANNEL](https://t.me/WarriorUnitsBots)
+├⛲ **Buy Source** : [Click Here](https://t.me/oo7jatji)
+├🎓 **Owner** : [STRANGER](https://t.me/oo7jatji)
+╰───────────────⍟
+"""
